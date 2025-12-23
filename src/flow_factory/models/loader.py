bfa68c0f7725636b54cb35f8c7478c8bc601d551
Jayce-Ping <315229706@qq.com>
2025-12-23 17:17:56 +0800

    Add MODEL_REGISTRY

--- src/flow_factory/models/loader.py
+++ src/flow_factory/models/loader.py
@@ -1,5 +1,6 @@
 # src/flow_factory/model/loader.py
 import logging
+import importlib
 from typing import Tuple
 from accelerate import Accelerator
 from .adapter import BaseAdapter
@@ -8,6 +9,11 @@ from ..hparams import *
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s')
 logger = logging.getLogger(__name__)
 
+# register map
+_MODEL_REGISTRY = {
+    "flux1": ("flux1", "Flux1Adapter"),
+    "z-image": ("z_image", "ZImageAdapter"),
+}
 
 def load_model(config : Arguments) -> BaseAdapter:
     """
@@ -25,11 +31,10 @@ def load_model(config : Arguments) -> BaseAdapter:
     
     logger.info(f"Loading model architecture: {model_type}...")
     
-    if model_type == "flux1":
-        from .flux1 import Flux1Adapter
-        return Flux1Adapter(config=config)
-    elif model_type == 'z-image':
-        from .z_image import ZImageAdapter
-        return ZImageAdapter(config=config)
-    else:
+    if model_type not in _MODEL_REGISTRY:
         raise NotImplementedError(f"Model type '{model_type}' is not supported yet.")
+
+    module_name, class_name = _MODEL_REGISTRY[model_type]
+    module = importlib.import_module(f'.{module_name}', package=__package__)
+    adapter_class = getattr(module, class_name)
+    return adapter_class(config=config)
