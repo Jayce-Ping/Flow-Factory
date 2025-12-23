16d8370ede53d119581f5c6497f05470095e1439
Jayce-Ping <315229706@qq.com>
2025-12-23 17:31:24 +0800

    update

--- src/flow_factory/train.py
+++ src/flow_factory/train.py
@@ -3,7 +3,7 @@ import os
 import argparse
 import logging
 from .hparams.args import Arguments
-from .trainers.loader import load_trainer
+from .trainers import load_trainer
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s')
 logger = logging.getLogger("flow_factory.train")
