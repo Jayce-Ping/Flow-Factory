2c6e819fccedf8b7e7603c1d41ce81432f8fde7c
Jayce <93087577+Jayce-Ping@users.noreply.github.com>
2026-02-05 20:08:16 +0800

    Add file path

--- src/flow_factory/ema/ema_utils.py
+++ src/flow_factory/ema/ema_utils.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# src/flow_factory/ema/ema_utils.py
 """
 Functional EMA Decay Utilities for Flow-Factory.
 
